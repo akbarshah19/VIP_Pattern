04fe1e6be6da601fe1d1a3fa28566088caf7613b
akbarshah19 <akbarshah19@icloud.com>
2025-03-19 21:09:27 +0500

    Initial Commit

---
+++ VIP_Pattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIP_Pattern
+//
+//  Created by Akbarshah Jumanazarov on 3/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
